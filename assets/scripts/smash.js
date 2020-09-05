7c629311749515ca4d172c2107afd324a0218d14
Adenikym <mm5adeniky@gmail.com>
2020-09-05 13:50:14 +0100

    weather page started

---
+++ assets/scripts/smash.js
@@ -0,0 +1,15 @@
+let modal=document.getElementById("my-modal");
+
+document.getElementById("close").addEventListener("click", function () {
+    modal.style.display="none"
+});
+
+document.getElementById("ham").addEventListener("click", function () {
+    modal.style.display="flex"
+});
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }

a6803f38c4cda017650e08dbf67457d04c6ea6a6
Adenikym <mm5adeniky@gmail.com>
2020-09-07 09:30:08 +0100

    service worker

---
+++ sw.js
@@ -0,0 +1,9 @@
+self.addEventListener("install", evt =>{
+    console.log("service worker installed")
+});
+
+//activate service worker
+
+self.addEventListener("activate", evt =>{
+    console.log("service worker activated")
+});
